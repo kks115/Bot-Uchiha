b53d0dd502c0e9bdd4c9eab96b48a1ccc58285ab
Rúben Silver <drogadinhosilver@gmail.com>
2025-12-02 09:03:31 +0100

    Update verifyController.ts

--- src/controllers/verifyController.ts
+++ src/controllers/verifyController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import logger from "../utils/logger";
 
 export function verifyToken(req: Request, res: Response) {
   const mode = req.query["hub.mode"];
@@ -6,9 +7,10 @@ export function verifyToken(req: Request, res: Response) {
   const challenge = req.query["hub.challenge"];
 
   if (mode === "subscribe" && token === process.env.VERIFY_TOKEN) {
-    console.log("Webhook verificado com sucesso.");
-    return res.status(200).send(challenge);
+    logger.info("Webhook verificado com sucesso");
+    return res.status(200).send(challenge as string);
   }
 
+  logger.warn("Falha na verificação do webhook");
   return res.sendStatus(403);
 }
